6862fa289025beafddb83399763ee5f1fc3199c9
FANG <1639579565@qq.com>
2022-04-12 19:23:10 +0800

    修改配置文件读取

--- iris-jwt/config/config.go
+++ iris-jwt/config/config.go
@@ -1,18 +1,42 @@
 package config
 
 import (
-	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
+
+	jsoniter "github.com/json-iterator/go"
 )
 
-func Init() error {
-	viper.AddConfigPath("./conf")
-	viper.SetConfigName("config")
-	viper.SetConfigType("yml")
+var Configuration = &configuration{}
 
-	err := viper.ReadInConfig()
+func init() {
+	// 指定配置文件
+	b, err := ioutil.ReadFile("conf/config.json")
 	if err != nil {
-		return err
+		log.Fatalln("failed to load the configuration")
 	}
+	// 配置文件加载到Configuration中
+	err = jsoniter.Unmarshal(b, Configuration)
+	if err != nil {
+		log.Fatalln("failed to load the configuration")
+	}
+}
 
-	return nil
+type configuration struct {
+	Port  string  `json:"port"`
+	Db    db      `json:"db"`
+	Redis redisDB `json:"redis"`
+}
+
+type db struct {
+	DBName   string `json:"dbname"`
+	Addr     string `json:"addr"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type redisDB struct {
+	Addr     string `json:"addr"`
+	Password string `json:"password"`
+	DBnum    int    `json:"db_num"`
 }
