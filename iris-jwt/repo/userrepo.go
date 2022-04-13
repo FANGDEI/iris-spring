c1231024c55c2ad79a7507e7491b892c39895723
FANG <1639579565@qq.com>
2022-04-13 17:36:21 +0800

    repo

--- iris-jwt/repo/userrepo.go
+++ iris-jwt/repo/userrepo.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"database/sql"
+	"errors"
 	"iris-jwt/database"
 	"iris-jwt/model"
 	"log"
@@ -11,22 +12,37 @@ import (
 
 var DB *sql.DB = database.GetMySQLDB()
 
-func SelectUserInfoById(id int) model.User {
+func SelectUserInfoById(id int) (model.User, error) {
 	var user model.User
 	row := DB.QueryRow("select id, username, password, email, phone, permission from user where id = ?", id)
 	err := row.Scan(&user.Id, &user.Username, &user.Password, &user.Email, &user.Phone, &user.Permission)
 	if err != nil {
 		log.Println("get the user information err: ", err)
+		return model.User{}, nil
 	}
-	return user
+	return user, nil
 }
 
-func SelectUserInformationByUsername(username string) model.User {
+func SelectUserInformationByUsername(username string) (model.User, error) {
 	var user model.User
 	row := DB.QueryRow("select id, username, password, email, phone, permission from user where username = ?", username)
 	err := row.Scan(&user.Id, &user.Username, &user.Password, &user.Email, &user.Phone, &user.Permission)
 	if err != nil {
 		log.Println("get the user information err: ", err)
+		return model.User{}, nil
 	}
-	return user
+	return user, nil
+}
+
+func InsertUser(username, password, email string) (int64, error) {
+	result, err := DB.Exec("insert into user (username, password, email, permission) values (?, ?, ?, ?)",
+		username, password, email, 0)
+	if err != nil {
+		return 0, err
+	}
+	cnt, _ := result.RowsAffected()
+	if cnt == 0 {
+		return cnt, errors.New("something wrong when insert user")
+	}
+	return cnt, nil
 }
