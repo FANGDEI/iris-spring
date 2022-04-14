4657ff81f34a0c9eb375f0993dd8bf468a15e99a
FANG <1639579565@qq.com>
2022-04-14 19:46:03 +0800

    grpc register

--- iris-jwt/service/register.go
+++ iris-jwt/service/register.go
@@ -1,4 +1,64 @@
 package service
 
+import (
+	"context"
+	re "iris-jwt/proto/register"
+	"iris-jwt/repo"
+	"iris-jwt/utils"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type RegisterServer struct {
+}
+
+func (s *RegisterServer) Register(ctx context.Context, request *re.RegisterRequest) (*re.RegisterResponse, error) {
+	return register(request.GetUsername(), request.GetPassword(), request.GetCode(), request.GetEmail()), nil
+}
+
+func register(username, password, code, email string) *re.RegisterResponse {
+	// 用户名不能为空
+	if username == "" {
+		return &re.RegisterResponse{Succeed: false, Message: "用户名不能为空"}
+	}
+
+	// 密码长度不能小于6位
+	if len(password) < 6 {
+		return &re.RegisterResponse{Succeed: false, Message: "密码不能小于六位，请重新输入密码"}
+	}
+
+	redisEmail, errGet := utils.GetValue(code)
+	if errGet != nil {
+		log.Println("failed to get value from redis.", errGet)
+		return &re.RegisterResponse{Succeed: false, Message: "未知原因注册失败"}
+	}
+
+	// 验证码错误
+	if redisEmail != email {
+		return &re.RegisterResponse{Succeed: false, Message: "验证码错误或已过期，请检查"}
+	}
+
+	// redis 中删除 code
+	errDeleteValue := utils.DeleteValue(code)
+	if errDeleteValue != nil {
+		log.Println("failed to delete value from redis")
+		return &re.RegisterResponse{Succeed: false, Message: "未知原因注册失败"}
+	}
+
+	// 密码加密
+	hash, errHash := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if errHash != nil {
+		log.Println("failed to get the hash with the password.")
+		return &re.RegisterResponse{Succeed: false, Message: "未知原因注册失败"}
+	}
+	encodePassword := string(hash)
+
+	cnt, errInsert := repo.InsertUser(username, encodePassword, email)
+	if errInsert != nil || cnt == 0 {
+		log.Println("failed to insert user into db.", errInsert)
+		return &re.RegisterResponse{Succeed: false, Message: "未知原因注册失败"}
+	}
+
+	return &re.RegisterResponse{Succeed: true, Message: "注册成功"}
 }
