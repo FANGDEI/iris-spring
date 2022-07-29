e1989bf235252a5cf78d4079711740883bd95ca6
FANG <1639579565@qq.com>
2022-07-30 01:36:38 +0800

    -

--- Distributed-Authentication-Service/src/auth/crypto/verify.go
+++
@@ -1,9 +0,0 @@
-package crypto
-
-import "golang.org/x/crypto/bcrypt"
-
-func Verify(encode, password string) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(encode), []byte(password))
-
-	return err == nil
-}
